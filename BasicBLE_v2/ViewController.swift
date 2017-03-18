0c54e8aef814136311d3b80d6d4fc18d2c5d4f12
Mark Illingworth <mark_illingworth@Marks-Mac-mini.local>
2017-03-18 12:36:44 +0800

    first commit

---
+++ BasicBLE_v2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BasicBLE_v2
+//
+//  Created by Mark Illingworth on 18/3/17.
+//  Copyright © 2017 Mark Illingworth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
